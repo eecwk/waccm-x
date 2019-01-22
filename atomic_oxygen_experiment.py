cc9ba3fdc2d1dcb7405bc4ab40f0e5443a399ff0
Christopher Kelly <eecwk@leeds.ac.uk>
2019-01-22 10:53:27 +0000

    Added CO and CO2 as profile outputs, and added a function for plotting their ratio.

--- atomic_oxygen_experiment.py
+++ atomic_oxygen_experiment.py
@@ -10,8 +10,8 @@ delta = unichr(916)
 k_B = 1.38064852e-23
 N_A = 6.02214086e+23
 R = 8.3144598
-species_list = ['atomic_oxygen', 'ozone', 'atomic_hydrogen', 'temperature', 'density']
-symbol_list = ['O', 'O3', 'H', 'T', 'n']
+species_list = ['atomic_oxygen', 'ozone', 'atomic_hydrogen', 'carbon_dioxide', 'carbon_monoxide', 'temperature', 'density']
+symbol_list = ['O', 'O3', 'H', 'CO2', 'CO', 'T', 'n']
 units_list = ['ppmv', '$\mathregular{cm^{-3}}$', 'K']
 
 fname_uni = netCDF4.Dataset('/nfs/a328/eecwk/earth_system_grid/ccsm4_monthly_ave/zonal_means/f.e20.FXSD.f19_f19.001.cam.h0.2000-01.nc', 'r', format='NETCDF4')
@@ -91,6 +91,13 @@ def calc_diff(param1, param2):
             diff[i,j] = ( (param2[i,j] - param1[i,j]) / param1[i,j] ) * 100
     return diff
 
+def calc_ratio(param1, param2, levs):
+    ratio = np.zeros([levs,96])
+    for i in range(0,levs):
+        for j in range(0,96): 
+            ratio[i,j] = param1[i,j] / param2[i,j]
+    return ratio
+    
 def calc_z3_zon_t_av_weighted(levs, lowlat, highlat):
     z3_zon_t_av = calc_z3_zon_t_av(levs)
     z3_zon_t_av_weighted = calc_cos_factor(z3_zon_t_av, levs, lowlat, highlat)
@@ -208,6 +215,13 @@ def plot_1d_multi(name, config, units, z3, species, lowlat, highlat, color, plot
             plt.xlim(0,20)
         if units == '$\mathregular{cm^{-3}}$':    
             plt.xlim(0,5.e+8)
+    if name == 'carbon_dioxide':
+        plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        if units == 'ppmv':
+            plt.xlim(0,500)
+            1==1
+        if units == '$\mathregular{cm^{-3}}$':    
+            plt.xlim(0,1.e+13)    
     if name == 'temperature':
         1==1
     if name == 'density':
@@ -218,6 +232,39 @@ def plot_1d_multi(name, config, units, z3, species, lowlat, highlat, color, plot
         plt.legend(loc=1)
     return
 
+def plot_1d_ratio(name, config, units, z3, species, lowlat, highlat, color, plot_no):
+    if plot_no > 5:
+        plot_no = plot_no - 6
+    plt.subplot(gs1[plot_no])
+    plt.title('%s%s to %s%s' %(lowlat_no, deg, highlat_no, deg), fontsize=14)
+    x = species[::-1]
+    y = z3[::-1]
+    plt.plot(x, y, color=color, label=config)
+    if plot_no == 0:
+        plt.ylabel('Altitude [km]', fontsize=12)
+        plt.tick_params(labelbottom='off')
+    if plot_no == 1:
+        plt.tick_params(labelleft='off')
+        plt.tick_params(labelbottom='off')
+    if plot_no == 2:
+        plt.tick_params(labelleft='off')
+        plt.tick_params(labelbottom='off')
+    if plot_no == 3:
+        plt.xlabel('%s' %name, fontsize=12)
+        plt.ylabel('Altitude [km]', fontsize=12)
+    if plot_no == 4:
+        plt.xlabel('%s' %name, fontsize=12)
+        plt.tick_params(labelleft='off')
+    if plot_no == 5:
+        plt.xlabel('%s' %name, fontsize=12)
+        plt.tick_params(labelleft='off')
+    plt.ylim(60,160)
+    plt.xlim(0,10)
+    if config == 'waccm-x' and plot_no == 2:
+        plt.legend(loc=1)
+    return
+
+    
 def plot_2d(name, z3, species, plot_no):
     plt.subplot(gs1[plot_no])
     x, y = np.meshgrid(lats, z3)
@@ -272,10 +319,15 @@ year = 2014
 month = 1
 name = species_list[4]
 symbol = symbol_list[4]
-units = units_list[1]
-chemistry = False
-global_only = True
+units = units_list[0]
+chemistry = True
+global_only = False
 save = False
+
+# For ratio:
+ratio = True
+name2 = species_list[3]
+symbol2 = symbol_list[3]
 
 if units == 'ppmv':
     units_print = 'ppmv'
@@ -301,7 +353,7 @@ else:
     step = 16
     a = 0
     b = 6
-
+'''
 #waccm_z3 = calc_z3_zon_mer_t_av(88)
 #waccmx_z3 = calc_z3_zon_mer_t_av(145)
 
@@ -321,29 +373,45 @@ for i in range(a,b):
     waccmx_z3_weighted = calc_z3_zon_t_av_weighted(145, lowlat, highlat)    
     if chemistry == True:
         if units == 'ppmv':
-            waccm_species_profile = calc_profiles(waccm_species, 88, lowlat, highlat)
+            #waccm_species_profile = calc_profiles(waccm_species, 88, lowlat, highlat)
             waccmx_species_profile = calc_profiles(waccmx_species, 145, lowlat, highlat)
         elif units == '$\mathregular{cm^{-3}}$':
-            waccm_species_profile = calc_conc_profiles(waccm_species, 88, lowlat, highlat)
+            #waccm_species_profile = calc_conc_profiles(waccm_species, 88, lowlat, highlat)
             waccmx_species_profile = calc_conc_profiles(waccmx_species, 145, lowlat, highlat)
     else:
         if symbol == 'T':
-            waccm_species_profile = calc_profiles(waccm_species, 88, lowlat, highlat)
+            #waccm_species_profile = calc_profiles(waccm_species, 88, lowlat, highlat)
             waccmx_species_profile = calc_profiles(waccmx_species, 145, lowlat, highlat) 
         elif symbol == 'n':
-            waccm_species_profile = calc_profiles(waccm_species, 88, lowlat, highlat)
+            #waccm_species_profile = calc_profiles(waccm_species, 88, lowlat, highlat)
             waccmx_species_profile = calc_profiles(waccmx_species, 145, lowlat, highlat)        
     if global_only == True:
-        plot_1d_global(name, 'waccm', units, waccm_z3_weighted, waccm_species_profile, 'k', 0)
+        #plot_1d_global(name, 'waccm', units, waccm_z3_weighted, waccm_species_profile, 'k', 0)
         plot_1d_global(name, 'waccm-x', units, waccmx_z3_weighted, waccmx_species_profile, 'b', 1)
     else:
-        plot_1d_multi(name, 'waccm', units, waccm_z3_weighted, waccm_species_profile, lowlat, highlat, 'k', i)
+        #plot_1d_multi(name, 'waccm', units, waccm_z3_weighted, waccm_species_profile, lowlat, highlat, 'k', i)
         plot_1d_multi(name, 'waccm-x', units, waccmx_z3_weighted, waccmx_species_profile, lowlat, highlat, 'b', i)
 if save == True:
     if global_only == True:
         plt.savefig('/nfs/a328/eecwk/waccm-x/figures/atomic_oxygen_experiment/john_ca_paper_JDmif_nad4cad7/%s/%s_month%s_profile_global_%s.jpg' %(year, name, month, units_print), bbox_inches='tight', dpi=300)
     else:
         plt.savefig('/nfs/a328/eecwk/waccm-x/figures/atomic_oxygen_experiment/john_ca_paper_JDmif_nad4cad7/%s/%s_month%s_profile_lat_bands_%s.jpg' %(year, name, month, units_print), bbox_inches='tight', dpi=300)
+'''
+# 1D Ratio Plot Code: WACCM-X only comparison workaround for missing WACCM species
+waccmx_species = calc_species_zon_av(symbol, 145)
+waccmx_species2 = calc_species_zon_av(symbol2, 145)
+waccmx_ratio = calc_ratio(waccmx_species, waccmx_species2, 145)
+for i in range(a,b):    
+    lowlat = i * step
+    highlat = (i * step) + step
+    lowlat_no = int((lowlat * 1.875) - 90)
+    highlat_no = int((highlat * 1.875) - 90)
+    waccm_z3_weighted = calc_z3_zon_t_av_weighted(88, lowlat, highlat)
+    waccmx_z3_weighted = calc_z3_zon_t_av_weighted(145, lowlat, highlat)    
+    waccmx_species_profile = calc_profiles(waccmx_ratio, 145, lowlat, highlat)      
+    plot_1d_ratio('%s / %s ratio' %(symbol, symbol2), 'waccm-x', units, waccmx_z3_weighted, waccmx_species_profile, lowlat, highlat, 'b', i)
+if save == True:
+    plt.savefig('/nfs/a328/eecwk/waccm-x/figures/atomic_oxygen_experiment/john_ca_paper_JDmif_nad4cad7/%s/%s_month%s_profile_lat_bands_%s.jpg' %(year, name, month, units_print), bbox_inches='tight', dpi=300)
 '''
 # 2D Plot Code
 # Currently for chemistry only
