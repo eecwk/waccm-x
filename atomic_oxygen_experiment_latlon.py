2f11879aae39454c81ad11f3bb22a609b1b17b39
Christopher Kelly <eecwk@leeds.ac.uk>
2019-02-18 09:41:36 +0000

    Added comment to warn that currently only valid for waccm-x output (as requires instantaneous).

--- atomic_oxygen_experiment_latlon.py
+++ atomic_oxygen_experiment_latlon.py
@@ -7,9 +7,13 @@ deg = unichr(176)
 p_int_w = np.zeros([96,144])
 p_int_wx = np.zeros([96,144])
 
+# NOTE 
+# WHEN PLOTTING INST. DATA, THIS SCRIPT CURRENTLY ONLY WORKS FOR WACCM-X DATA
+# NEED TO SELECT TIME CORRECTLY WHEN USING SUITABLE INST. WACCM DATA
+
 def get_fixed_variables(config, year, month, symbol):
     if config == 'waccmx':
-        fname = netCDF4.Dataset('/nfs/a328/eecwk/earth_system_grid/ccsm4_monthly_ave/f.e20.FXSD.f19_f19.001.cam.h0.%s-%s.nc' %(year, month), 'r', format='NETCDF4')
+        fname = netCDF4.Dataset('/nfs/a328/eecwk/earth_system_grid/ccsm4_daily_inst/f.e20.FXSD.f19_f19.001.cam.h2.%s-%s-0%s-00000.nc' %(year, month, start_day), 'r', format='NETCDF4')
     if config == 'waccm':
         fname = netCDF4.Dataset('/nfs/a265/earfw/SD_WACCM4/john_ca_paper_JDmif_nad4cad7.cam2.h0.%s-%s.nc' %(year, month), 'r', format='NETCDF4')  
     var = fname.variables[symbol][:]
@@ -20,11 +24,11 @@ def get_variables(config, levs, year, month, symbol):
     tracer_dat = np.zeros([1,levs,96,144])
     tracer = np.zeros([levs,96,144])
     if config == 'waccmx':
-        fname = netCDF4.Dataset('/nfs/a328/eecwk/earth_system_grid/ccsm4_monthly_ave/f.e20.FXSD.f19_f19.001.cam.h0.%s-%s.nc' %(year, month), 'r', format='NETCDF4')
+        fname = netCDF4.Dataset('/nfs/a328/eecwk/earth_system_grid/ccsm4_daily_inst/f.e20.FXSD.f19_f19.001.cam.h2.%s-%s-0%s-00000.nc' %(year, month, start_day), 'r', format='NETCDF4')
     if config == 'waccm':
         fname = netCDF4.Dataset('/nfs/a265/earfw/SD_WACCM4/john_ca_paper_JDmif_nad4cad7.cam2.h0.%s-%s.nc' %(year, month), 'r', format='NETCDF4')
     tracer_dat = fname.variables[symbol][:]
-    tracer = np.mean(tracer_dat, axis=0)
+    tracer = tracer_dat[2,:,:,:]
     fname.close()
     return tracer
 
@@ -44,7 +48,8 @@ def interp_tracer(tracer, levs, p_int):
 
 def plot_2d_latlon(tracer, diffs, name, units):
     x, y = np.meshgrid(lons, lats)
-    plt.contourf(x[:,:], y[:,:], tracer[:,:], diffs)
+    #plt.contourf(x[:,:], y[:,:], tracer[:,:], diffs)
+    plt.contourf(x[:,:], y[:,:], tracer[:,:])
     plt.xlabel('Longitude [%s]' %deg, fontsize=12)
     plt.ylabel('Latitude [%s]' %deg, fontsize=12)
     altitude_km = altitude/1000
@@ -56,8 +61,8 @@ def plot_2d_latlon(tracer, diffs, name, units):
 def plot_2d_latlon_sub(config, tracer, diffs, name, units, plot_no):
     plt.subplot(gs1[plot_no])
     x, y = np.meshgrid(lons, lats)
-    ax = plt.contourf(x[:,:], y[:,:], tracer[:,:], diffs)
-    #ax = plt.contourf(x[:,:], y[:,:], tracer[:,:])
+    #ax = plt.contourf(x[:,:], y[:,:], tracer[:,:], diffs)
+    ax = plt.contourf(x[:,:], y[:,:], tracer[:,:])
     plt.xlabel('Longitude [%s]' %deg, fontsize=12)
     altitude_km = altitude/1000
     plt.title('%s %s-%s (%s km)' %(config, year, month, altitude_km), fontsize=14)
@@ -72,25 +77,27 @@ def plot_2d_latlon_sub(config, tracer, diffs, name, units, plot_no):
 
 year = '2014'
 month = '01'
+start_day = 4
 #altitude = 75000
 #altitude = 85000
-altitude = 90000
+altitude = 60000
 #altitude = 95000
 #altitude = 120000
 
-levs_w = get_fixed_variables('waccm', year, month, 'lev')
+#levs_w = get_fixed_variables('waccm', year, month, 'lev')
 levs_wx = get_fixed_variables('waccmx', year, month, 'lev')
 lats = get_fixed_variables('waccmx', year, month, 'lat')
 lons = get_fixed_variables('waccmx', year, month, 'lon')
 
-z3_w = get_variables('waccm', 88, year, month, 'Z3')
-p_int_w[:,:] = interp_p(altitude, z3_w, levs_w)
+#z3_w = get_variables('waccm', 88, year, month, 'Z3')
+#p_int_w[:,:] = interp_p(altitude, z3_w, levs_w)
 z3_wx = get_variables('waccmx', 145, year, month, 'Z3')
 p_int_wx[:,:] = interp_p(altitude, z3_wx, levs_wx)
-
+'''
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(9,3))
 gs1 = gridspec.GridSpec(1, 2)
 gs1.update(wspace=0.1, hspace=0.1)
+'''
 '''
 #diffs = np.arange(10000,90000,2500)
 diffs = np.arange(160000,360000,10000)
@@ -102,7 +109,7 @@ o_wx_int = interp_tracer(o_wx*1.e+6, levs_wx, p_int_wx)
 plot_2d_latlon_sub('waccmx', o_wx_int, diffs, 'atomic_oxygen', 'ppmv', 1)
 plt.savefig('/nfs/a328/eecwk/waccm-x/figures/atomic_oxygen_experiment/john_ca_paper_JDmif_nad4cad7/%s/atomic_oxygen_month%s_120km_latlon_ppmv.jpg' %(year, month), bbox_inches='tight', dpi=300)
 '''
-
+'''
 #diffs = np.arange(0,3.2,0.1)
 #diffs = np.arange(0,1.2,0.05)
 diffs = np.arange(0,3.6,0.1)
@@ -112,6 +119,11 @@ plot_2d_latlon_sub('waccm', o3_w_int, diffs, 'ozone', 'ppmv', 0)
 o3_wx = get_variables('waccmx', 145, year, month, 'O3')
 o3_wx_int = interp_tracer(o3_wx*1.e+6, levs_wx, p_int_wx)
 plot_2d_latlon_sub('waccmx', o3_wx_int, diffs, 'ozone', 'ppmv', 1)
-#plt.savefig('/nfs/a328/eecwk/waccm-x/figures/atomic_oxygen_experiment/john_ca_paper_JDmif_nad4cad7/%s/ozone_month%s_90km_latlon_ppmv.jpg' %(year, month), bbox_inches='tight', dpi=300)
+plt.savefig('/nfs/a328/eecwk/waccm-x/figures/atomic_oxygen_experiment/john_ca_paper_JDmif_nad4cad7/%s/ozone_month%s_90km_latlon_ppmv.jpg' %(year, month), bbox_inches='tight', dpi=300)
+'''
+diffs = np.arange(160000,360000,10000)
+o_wx = get_variables('waccmx', 145, year, month, 'O')
+o_wx_int = interp_tracer(o_wx*1.e+6, levs_wx, p_int_wx)
+plot_2d_latlon(o_wx_int, diffs, 'atomic_oxygen', 'ppmv')
 
 plt.show()
