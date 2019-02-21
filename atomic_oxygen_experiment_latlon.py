43992108fb46481d482bc8e42afaaca5e4fd45bf
Christopher Kelly <eecwk@leeds.ac.uk>
2019-02-21 14:50:11 +0000

    Now plots concentration lat/lon panel plots for waccm-x, msis and saber for fixed specified altitude.

--- atomic_oxygen_experiment_latlon.py
+++ atomic_oxygen_experiment_latlon.py
@@ -6,99 +6,200 @@ import matplotlib.gridspec as gridspec
 N_A = 6.02214086e+23
 R = 8.3144598
 deg = unichr(176)
-p_int_w = np.zeros([96,144])
-p_int_wx = np.zeros([96,144])
 
-def get_fixed_variables(config, year, month, symbol):
-    if config == 'waccmx':
-        fname = netCDF4.Dataset('/nfs/a328/eecwk/earth_system_grid/ccsm4_daily_inst/f.e20.FXSD.f19_f19.001.cam.h2.%s-%s-0%s-00000.nc' %(year, month, start_day), 'r', format='NETCDF4') 
-    var = fname.variables[symbol][:]
-    fname.close()
-    return var
+years = [2009, 2009, 2009, 2009, 2014, 2014, 2014, 2014]
+months = [3, 6, 9, 12, 3, 6, 9, 12]
+days = [20, 21, 22, 21, 20, 21, 23, 21]
+waccmx_start_days = [14, 20, 19, 19, 15, 21, 20, 20]
+doy = [79, 172, 265, 355, 79, 172, 266, 355]
 
-def get_variables(config, levs, year, month, symbol):
-    tracer_dat = np.zeros([1,levs,96,144])
-    tracer = np.zeros([levs,96,144])
-    if config == 'waccmx':
-        fname = netCDF4.Dataset('/nfs/a328/eecwk/earth_system_grid/ccsm4_daily_inst/f.e20.FXSD.f19_f19.001.cam.h2.%s-%s-0%s-00000.nc' %(year, month, start_day), 'r', format='NETCDF4')
-    tracer_dat = fname.variables[symbol][:]
-    tracer = tracer_dat[2,:,:,:]
+altitude = 100
+event = 0
+set_year = years[event]
+set_month = months[event]
+set_day = days[event]
+waccmx_start_day = waccmx_start_days[event]
+saber_doy = doy[event]
+waccmx_select_day = set_day - waccmx_start_day
+
+# SABER
+fname = netCDF4.Dataset('/nfs/a265/earfw/CHRIS/SABER/NIGHTLY/atox_athy_night_YY%s_V5.3_fixedfnight_SV2.nc' %set_year, 'r', format='NETCDF4')
+o = fname.variables['qatox'][:]
+T = fname.variables['ktemp'][:]
+p = fname.variables['pressure'][:]
+lats_saber = fname.variables['lat'][:]
+lons_saber = fname.variables['lon'][:]
+alt = fname.variables['alt'][:]
+year = fname.variables['year'][:]
+day = fname.variables['day'][:]
+fname.close
+lats_bin_mid_saber = np.arange(-87.5, 92.5, 5)
+
+def make_saber_array(tracer):
+    tracer_bin = np.zeros([16,36,36])
+    for j in range(0,16):
+        for k in range(0,36):
+            k_min = (k*5) - 90
+            k_max = (k*5) - 85
+            for l in range(0,36):            
+                l_min = (l*5)
+                l_max = (l*10) + 10
+                time_lat_lon_bin = np.append(np.where((lats_saber > k_min) & (lats_saber < k_max) & (lons_saber > l_min) & (lons_saber < l_max) & (year == set_year) & (day == saber_doy)), True) 
+                tracer_scans = tracer[time_lat_lon_bin, j]                
+                tracer_scans_good = np.array([])
+                for i in range(0, len(tracer_scans)):
+                    if tracer_scans[i] > 0:
+                        tracer_scans_good = np.append(tracer_scans_good, tracer_scans[i])
+                if len(tracer_scans_good) < 2:
+                    tracer_scans_good = np.nan
+                tracer_scans_mean = np.mean(tracer_scans_good)
+                tracer_bin[j,k,l] = tracer_scans_mean                               
+    return tracer_bin
+
+def interp_saber_p(altitude, gpheight, pressure):        
+    p_int = np.zeros([36,36])
+    for i in range(0,36):
+        for j in range(0,36):
+            p_int[i,j] = np.interp(altitude, gpheight[:,i,j][::-1], pressure[:][::-1])     
+    return p_int
+
+def interp_saber_tracer(tracer, pressure, p_int):
+    tracer_int = np.zeros([36,36])
+    for i in range(0,36):
+        for j in range(0,36):    
+            tracer_int[i,j] = np.interp(p_int[i,j], pressure[:], tracer[:,i,j])
+    return tracer_int
+
+def calc_saber_conc_profile(tracer, p_int, T_int):
+    tracer_conc = np.zeros([36,36])  
+    for i in range(0,36):
+        for j in range(0,36):
+            if T_int[i,j] > 0:
+                tracer_conc[i,j] = (tracer[i,j] * 1.e-6 * N_A * 100 * p_int[i,j]) / (R * T_int[i,j])
+            else:
+                tracer_conc[i,j] = np.nan
+    return tracer_conc
+
+# WACCM-X
+fname = netCDF4.Dataset('/nfs/a328/eecwk/earth_system_grid/ccsm4_daily_inst/f.e20.FXSD.f19_f19.001.cam.h2.2014-01-04-00000.nc', 'r', format='NETCDF4') 
+levs_waccmx = fname.variables['lev'][:]
+lats_waccmx = fname.variables['lat'][:]
+lons_waccmx = fname.variables['lon'][:]
+fname.close()
+
+def make_waccmx_array(symbol, factor):
+    fname = netCDF4.Dataset('/nfs/a328/eecwk/earth_system_grid/ccsm4_daily_inst/f.e20.FXSD.f19_f19.001.cam.h2.%s-0%s-%s-00000.nc' %(set_year, set_month, waccmx_start_day), 'r', format='NETCDF4')
+    tracer_dat = np.zeros([7,145,96,144])
+    tracer_dat = fname.variables[symbol][:]*factor    
+    tracer = tracer_dat[waccmx_select_day,:,:,:]    
     fname.close()
     return tracer
 
-def interp_p(altitude, gpheight, levs):        
+def interp_waccmx_p(altitude, gpheight, levs):        
     p_int = np.zeros([96,144])
     for i in range(0,96):
         for j in range(0,144):
             p_int[i,j] = np.interp(altitude, gpheight[:,i,j][::-1], levs[:][::-1])     
     return p_int
 
-def interp_tracer(tracer, levs, p_int):
+def interp_waccmx_tracer(tracer, pressure, p_int):
     tracer_int = np.zeros([96,144])
     for i in range(0,96):
         for j in range(0,144):    
-            tracer_int[i,j] = np.interp(p_int[i,j], levs[:], tracer[:,i,j])
+            tracer_int[i,j] = np.interp(p_int[i,j], pressure[:], tracer[:,i,j])
     return tracer_int
 
-def calc_conc_profile(tracer):
-    T_wx = np.zeros([1,145,96,144])
-    T_wx = get_variables('waccmx', 145, year, month, 'T')
-    T_wx = interp_tracer(T_wx, levs_wx, p_int_wx)
+def calc_waccmx_conc_profile(tracer, p_int, T_int):
     tracer_conc = np.zeros([96,144])  
     for i in range(0,96):
         for j in range(0,144):
-            tracer_conc[i,j] = (tracer[i,j] * 1.e-6 * N_A * 100 * p_int_wx[i,j]) / (R * T_wx[i,j])
+            tracer_conc[i,j] = (tracer[i,j] * 1.e-6 * N_A * 100 * p_int[i,j]) / (R * T_int[i,j])
     return tracer_conc
 
-def plot_2d_latlon(tracer, diffs, name, units):
-    x, y = np.meshgrid(lons, lats)
-    #plt.contourf(x[:,:], y[:,:], tracer[:,:], diffs)
-    plt.contourf(x[:,:], y[:,:], tracer[:,:])
-    plt.xlabel('Longitude [%s]' %deg, fontsize=12)
-    plt.ylabel('Latitude [%s]' %deg, fontsize=12)
-    altitude_km = altitude/1000
-    plt.title('%s/%s/0%s, night, (%s km)' %(year, month, set_day, altitude_km), fontsize=14)
-    cbar = plt.colorbar(orientation='vertical', cmap=plt.get_cmap('viridis'))
-    cbar.set_label('%s [%s]' %(name, units), fontsize=12)
-    plt.show()
+# NRLMSISE
+fname = netCDF4.Dataset('/nfs/a328/eecwk/nrlmsise/output_data/nrlmsise_ghp7_20090320-00000.nc', 'r', format='NETCDF4')
+levs_msis = fname.variables['level'][:]
+lats_msis = fname.variables['latitude'][:]
+lons_msis = fname.variables['longitude'][:]
+fname.close()
 
-def plot_2d_latlon_sub(config, tracer, diffs, name, units, plot_no):
+def make_msis_array(symbol):  
+    fname = netCDF4.Dataset('/nfs/a328/eecwk/nrlmsise/output_data/nrlmsise_ghp7_%s0%s%s-00000.nc' %(set_year, set_month, set_day), 'r', format='NETCDF4') 
+    tracer_dat = np.zeros([1,145,96,144])
+    tracer_dat = fname.variables[symbol][:]
+    tracer = tracer_dat[0,:,:,:]
+    fname.close()
+    return tracer
+
+def interp_msis_p(altitude, gpheight, levs):        
+    p_int = np.zeros([96,144])
+    for i in range(0,96):
+        for j in range(0,144):
+            p_int[i,j] = np.interp(altitude, gpheight[:,i,j][::-1], levs[:][::-1])     
+    return p_int
+
+def interp_msis_tracer(tracer, pressure, p_int):
+    tracer_int = np.zeros([96,144])
+    for i in range(0,96):
+        for j in range(0,144):    
+            tracer_int[i,j] = np.interp(p_int[i,j], pressure[:], tracer[:,i,j])
+    return tracer_int
+
+def calc_msis_conc_profile(tracer, p_int, T_int):
+    tracer_conc = np.zeros([96,144])  
+    for i in range(0,96):
+        for j in range(0,144):
+            tracer_conc[i,j] = (tracer[i,j] * 1.e-6 * N_A * 100 * p_int[i,j]) / (R * T_int[i,j])
+    return tracer_conc
+
+# Plot
+def plot_2d_latlon_sub(tracer, lats, lons, diffs, config, name, units, plot_no):
+    plt.subplot(gs1[plot_no])
     x, y = np.meshgrid(lons, lats)
-    #ax = plt.contourf(x[:,:], y[:,:], tracer[:,:], diffs)
-    ax = plt.contourf(x[:,:], y[:,:], tracer[:,:])
+    ax = plt.contourf(x[:,:], y[:,:], tracer[:,:], diffs)
+    #ax = plt.contourf(x[:,:], y[:,:], tracer[:,:])
     plt.xlabel('Longitude [%s]' %deg, fontsize=12)
-    altitude_km = altitude/1000
-    plt.title('%s %s-%s (%s km)' %(config, year, month, altitude_km), fontsize=14)
+    plt.title('%s' %config, fontsize=14)
     if plot_no == 0:
         plt.ylabel('Latitude [%s]' %deg, fontsize=12)
     if plot_no == 1:
+        plt.tick_params(labelleft='off')
+    if plot_no == 2:
         plt.tick_params(labelleft='off')
         cbar_ax = fig.add_axes([0.94, 0.15, 0.02, 0.7])
         cbar = fig.colorbar(ax, cax=cbar_ax, orientation='vertical')
         cbar.set_label('%s [%s]' %(name, units), fontsize=12)
         cbar.ax.tick_params(labelsize=12)
+    return
 
-year = '2014'
-month = '01'
-start_day = 4
-set_day = 6
-altitude = 100000
+saber_alt = make_saber_array(alt)
+saber_o = make_saber_array(o)
+saber_T = make_saber_array(T)
+saber_p_int = interp_saber_p(altitude, saber_alt, p)
+saber_o_int = interp_saber_tracer(saber_o, p, saber_p_int)
+saber_T_int = interp_saber_tracer(saber_T, p, saber_p_int)
+saber_o_int_conc = calc_saber_conc_profile(saber_o_int, saber_p_int, saber_T_int)
 
-levs_wx = get_fixed_variables('waccmx', year, month, 'lev')
-lats = get_fixed_variables('waccmx', year, month, 'lat')
-lons = get_fixed_variables('waccmx', year, month, 'lon')
+waccmx_alt = make_waccmx_array('Z3', 1.e-3)
+waccmx_o = make_waccmx_array('O', 1)
+waccmx_T = make_waccmx_array('T', 1)
+waccmx_p_int = interp_waccmx_p(altitude, waccmx_alt, levs_waccmx)
+waccmx_o_int = interp_waccmx_tracer(waccmx_o, levs_waccmx, waccmx_p_int)
+waccmx_T_int = interp_waccmx_tracer(waccmx_T, levs_waccmx, waccmx_p_int)
+waccmx_o_int_conc = calc_waccmx_conc_profile(waccmx_o_int, waccmx_p_int, waccmx_T_int)
 
-z3_wx = get_variables('waccmx', 145, year, month, 'Z3')
-p_int_wx[:,:] = interp_p(altitude, z3_wx, levs_wx)
-o_wx = get_variables('waccmx', 145, year, month, 'O')
-#o_wx_int = interp_tracer(o_wx*1.e+6, levs_wx, p_int_wx)
-o_wx_int = interp_tracer(o_wx, levs_wx, p_int_wx)
-o_wx_int_conc = calc_conc_profile(o_wx_int)
+msis_alt = make_msis_array('Z3')
+msis_o = make_msis_array('O')
+msis_T = make_msis_array('T')
+msis_p_int = interp_msis_p(altitude, msis_alt, levs_msis)
+msis_o_int = interp_msis_tracer(msis_o, levs_msis, msis_p_int)
 
-fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(9,6))
-gs1 = gridspec.GridSpec(1, 1)
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10,4))
+gs1 = gridspec.GridSpec(1, 3)
 gs1.update(wspace=0.1, hspace=0.1)
-diffs = np.arange(160000,360000,10000)
-#plot_2d_latlon(o_wx_int, diffs, 'atomic_oxygen', 'ppmv')
-plot_2d_latlon(o_wx_int_conc, diffs, 'atomic_oxygen', 'cm-3')
+plt.suptitle('%s/0%s/%s, night, (%s km)' %(set_year, set_month, set_day, altitude), fontsize=16)
+diffs = np.arange(1.e+11,7.e+11,2.e+10)
+plot_2d_latlon_sub(waccmx_o_int_conc, lats_waccmx, lons_waccmx, diffs, 'WACCM-X', 'atomic_oxygen', 'cm-3', 0)
+plot_2d_latlon_sub(msis_o_int, lats_msis, lons_msis, diffs, 'MSIS', 'atomic_oxygen', 'cm-3', 1)
+plot_2d_latlon_sub(saber_o_int_conc, np.arange(-87.5,92.5,5), np.arange(5,365,10), diffs, 'SABER', 'atomic_oxygen', 'cm-3', 2)
 plt.show()
